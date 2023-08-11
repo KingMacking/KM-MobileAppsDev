74859e81eda5244934b8449075e10a96950ca335
King Macking <matiasatzori@gmail.com>
2023-08-10 21:03:10 -0300

    OTHERS: Remove google api key

--- src/constants/maps/index.js
+++ src/constants/maps/index.js
@@ -1,4 +1,4 @@
-export const GOOGLE_API = 'AIzaSyDVZyQxrKeX2pbx1HWSq4-E5_IFMFh_s38';
+export const GOOGLE_API = '';
 export const URL_MAPS = ({ lat, lng, zoom }) =>
     `https://maps.googleapis.com/maps/api/staticmap?center=${lat},${lng}&zoom=${zoom}&size=600x300&maptype=roadmap&markers=color:red%7Clabel:A%7C${lat},${lng}&key=${GOOGLE_API}`;
 
